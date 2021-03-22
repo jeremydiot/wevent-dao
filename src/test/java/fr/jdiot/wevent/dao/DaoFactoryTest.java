b27e3dfec430a3318007ba7ea812d27ef1d9722c
Jeremy Diot <diot.jeremy@outlook.com>
2021-03-22 16:57:47 +0100

    delete unused files

--- src/test/java/fr/jdiot/wevent/dao/DaoFactoryTest.java
+++ src/test/java/fr/jdiot/wevent/dao/DaoFactoryTest.java
@@ -1,13 +1,16 @@
 package fr.jdiot.wevent.dao;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
-public class AppTest {
+public class DaoFactoryTest {
+
 
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
 
 }
